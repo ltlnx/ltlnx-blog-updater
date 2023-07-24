a0be491fd2b479384b98f235eda8ef88c18cc2c3
Wen-Wei Kao <ltlnx@gmail.com>
2023-07-24 13:47:43 +0800

    update script

--- README.md
+++ README.md
@@ -2,15 +2,31 @@
 This is my blog updater: it takes a source directory with Markdown files, a "header" and a "footer", converts them to HTML, and copies them to the desired destination directory.
 
 ### Prerequisites
-The script currently assumes a UNIX-like environment with GNU `grep` and `sed`. I may turn some expressions into perl expressions for better compatibility. Also, `pandoc` should be in your path; please see [pandoc.org](https://pandoc.org) for install instructions.
+- a shell with `echo -e` support (i.e. most Linux distros with GNU or Busybox utils)
+- GNU grep (or a grep supporting `-P`)
+- GNU sed
+- Pandoc
 
-You should set up two directories in the same directory as the script: `src` and `pages`. Also in the same directory, add a `style.css` file if you want to apply styles to pages. In the `src` directory, there should be two files: `header` and `footer`, which corresponds to the top and bottom of all HTML files.
+There may be something missing here but in general, a Linux system with pandoc installed would probably work. (If not, create an issue.)
 
-In the `src` directory, you may add directories which would become categories after you run the script. In every directory there may be a `desc` file, which would contain the description of the category in Markdown.
+### Setup (on Linux)
+1. Open a terminal and clone this repo.
+2. Go into the cloned directory: `cd ltlnx-blog-updater`
+3. Make sure the script is executable: `chmod u+x update.sh`
+4. Run it : `./update.sh`
 
-This repo contains a minimal example for all of the above. After cloning, first modify two places: the value of `vroot` in `update.sh`, and the `href` value in `style.css` to reflect the actual location of the files, then run `update.sh` directly and see what appears in the `pages` directory. After that, you can modify the script and files to fit your needs.
+At this point the script would create a file named `updater_config` in the directory. Please change the details to your liking.
 
-For more info see my blog post about it: <https://ltlnx.tw/software/blog-generator.html>
+### Writing blog posts and updating files
+Change into the source directory `src`.
+
+- To form a "category", create a new directory with the category name. The first letter will be capitalized and hyphens will be turned into spaces. e.g. `public-logs` becomes `Public Logs`.
+- To add a date to links on the index pages, add a line with `Last updated: <date>`. You can replace `updated: ` with another word or add underscores (`_`) around them and it'll still work. See the demo posts in the `src` directory for more info.
+- To "theme" your site, add a CSS file, preferably named `style.css`, in the source directory. A minimal example is included. You can inspect the generated HTML (with Firefox's inspector, a text editor or whatever) and determine which target to write styles for.
+- To add a description on the top of index pages, add a file named "desc" in the directories you want a description, and add Markdown content to the file. Again, see the `desc` files in the source directory for an example.
+
+### RSS
+This script also generates an RSS feed named "rssfeed.xml" in the destination directory. You can decide what to do with it (add a link to it maybe?)
 
 ### Contributing / Bug reports
 This script currently doesn't accept pull requests, but you're welcome to report bugs in Github Issues or directly to ltlnx dot tw at gmail dot com. Thanks for discovering and using my script!
--- update.sh
+++ update.sh
@@ -3,80 +3,91 @@
 # MIT licensed
 
 main() {
-    # variables
-    # naming scheme: global vars start with v; part of paths have the suffix "name"
     vargs=$(argsparse "$@") || die "Failed to parse arguments"
-    vblogname="ltlnx"
-    vroot="/home/ltlnx/Documents/Blog"
-    vsrcname="src"
-    vdstname="pages"
-    vstyle="$vroot/style.css"
-    vbackupdirname="backup"
-    vheadername="header"
-    vfootername="footer"
-    vreleaseheaderprefix="release-"
-    vdotfiles="$vroot/$vsrcname/.files"
-    vmdconvcommand="$vroot/md2html"
-    which $vmdconvcommand >/dev/null || which pandoc >/dev/null \
-        && vmdconvcommand="pandoc -f markdown -t html" \
-        || die "Markdown conversion program not found"
-    # start profiler
-    # PS4='+ $(date "+%s.%N")\011 '
-    # exec 3>&2 2>$vroot/profiler/bashstart.$$.log
-    # set -x
-    # since we are moving residue files into this dir instead of deleting
-    # them, the script will create the dir if it doesn't exist to minimize
-    # file loss
-    mkdir -p "$vroot/$vbackupdirname"
+    # test if a config is present in the same dir as the update script
+    # if not write an example config to the same directory as the script
+    vconfigname="updater_config"
+    vconfig=$(echo "$0" | sed "s|$(basename "$0")|$vconfigname|")
+    test -f "$vconfig" && source "$vconfig" || script_init
+    test_result="$(run_tests)"
+    test -z "$test_result" || die "$test_result"
+    # quick commands
+    case $1 in
+        deploy)
+            test "$2" || die "Please input a commit message"
+            test "$3" && die "Please put the commit message in quotes"
+            cd "$vroot"/"$vdstname" || exit 1
+            git add .
+            git commit -m "$2"
+            git push
+            cd - || exit 1
+            exit 0
+            ;;
+        serve)
+            cd "$vroot"/"$vdstname" || exit 1
+            python3 -m http.server || die "Your system probably does not have Python 3 installed. Please change the line invoking python3 into your own web server command, or install Python 3."
+            cd - || exit 1
+            exit 0
+            ;;
+    esac
+    vdotfiles="$vroot/.files"
+    # start profiler if variable is set
+    if (echo "$vargs" | grep -q 'p'); then
+        PS4='+ $(date "+%s.%N")\011 '
+        mkdir -p "$vroot"/profiler
+        exec 3>&2 2>"$vroot"/profiler/bashstart.$$.log
+        set -x
+    fi
     vcurrentfilelist="$(find_exclusion "$vroot/$vsrcname")"
     vcopylist=""
     vremovelist=""
-    visrelease="$(echo $vargs | grep -q "r" && echo "y")"
-    test_dirs || die "Required directories do not exist"
+    vresiduefilelist=""
     # test if src/.files exists, if not create an empty one for diffing
-    test -f $vdotfiles || echo "" > $vdotfiles \
+    test -f "$vdotfiles" || echo "" > "$vdotfiles" \
         || die "Failed to create .files"
-    # set vheader and vfooter according to whether the user specify to
-    # build a release
-    if (test "$visrelease" = "y"); then
-        vheader="$vroot/$vsrcname/$vreleaseheaderprefix$vheadername"
-        vfooter="$vroot/$vsrcname/$vreleaseheaderprefix$vfootername"
-        mv $vdotfiles "$vdotfiles.tmp"
-        echo "" > $vdotfiles
-    else
-        vheader="$vroot/$vsrcname/$vheadername"
-        vfooter="$vroot/$vsrcname/$vfootername"
-        # test if the header or footer changed; if so rebuild the whole thing
-        if (test $vheader -nt $vdotfiles || test $vfooter -nt $vdotfiles); then
-            echo "" > $vdotfiles
-        fi
+    cp "$vdotfiles" "$vdotfiles.old"
+    vheader="$vroot/$vheadername"
+    vfooter="$vroot/$vfootername"
+    # test if the header or footer changed; if so rebuild the whole thing
+    if test "$vheader" -nt "$vdotfiles" \
+        || test "$vfooter" -nt "$vdotfiles" \
+        || test "$vconfig" -nt "$vdotfiles"; then
+        echo "" > "$vdotfiles"
     fi
     vfulldirlist="$(get_dirs "$vcurrentfilelist" | grep -v "/res")"
-    vfdiff=$(diff $vdotfiles <(echo "$vcurrentfilelist"))
+    vfdiff=$(diff "$vdotfiles" <(echo "$vcurrentfilelist"))
     vfold=$(echo -e "$vfdiff" | grep -Po "^< \K.*$")
     vfnew="$(echo -e "$(echo -e "$vfdiff" | grep -Po "^> \K.*$")\n$(find_exclusion "$vroot/$vsrcname" -newer "$vdotfiles")" | grep . | sort -u)"
     # mandate the rebuild of the main index when there are changed files
     # this would stay here until I think of a better algorithm
-    if (echo -e "$vfnew\n$vfold" | grep -q "$vroot/$vsrcname"); then
+    if ! (echo -e "$vfnew\n$vfold" | grep -q "$vroot/$vsrcname" | grep -vq "$vroot/$vsrcname$"); then
         vdirlist="$vroot/$vsrcname"
     fi
-    vdirlist="$vdirlist $(get_dirs "$(cat <(echo -e "$vfnew") <(echo -e "$vfold"))")" \
-        || vdirlist="$vfulldirlist"
+    vdirlist="$vdirlist $(get_dirs "$(cat <(echo -e "$vfnew") <(echo -e "$vfold"))" \
+        | grep -v "/res")" || vdirlist="$vfulldirlist"
     # build html files for new or changed markdown files
     if [ "$vfnew" ]; then
         # we're going to build all file indices to make sure indice builds
         # have the correct info, but only build indices for dirs with changed files
         for dir1 in $vfulldirlist; do
-            build_fileindex $dir1 > "$dir1/.fileindex"
+            build_fileindex "$dir1" > "$dir1/.fileindex"
         done
         for dir2 in $vdirlist; do
             vcopylist="$vcopylist${dir2}/index.html\n"
-            build_index $dir2 > "$dir2/index.md"
-            mdtohtml "$dir2/index.md" > "$dir2/index.html" || die "Failed to convert $dir2 index to HTML"
+            build_index "$dir2" > "$dir2/index.md"
+            if $(mdtohtml "$dir2/index.md" > "$dir2/index.html"); then
+                vresiduefilelist="$vresiduefilelist$dir2/index.html\n$dir2/index.md\n"
+            else
+                die "Failed to convert $dir2 index to HTML"
+            fi
         done
         for mdfile in $(echo -e "$vfnew" | grep ".md$"); do
-            htmlfilename="$(echo $mdfile | mdfilenametohtml)"
-            mdtohtml $mdfile > $htmlfilename || die "Failed to convert $mdfile to HTML"
+            htmlfilename="$(echo "$mdfile" | mdfilenametohtml)"
+            if $(mdtohtml "$mdfile" > "$htmlfilename"); then
+                vresiduefilelist="$vresiduefilelist$htmlfilename\n"
+            else
+                die "Failed to convert $mdfile to HTML"
+            fi
             vcopylist="$vcopylist$htmlfilename\n"
         done
         vcopylist="$vcopylist$(echo -e "$vfnew" | grep -v ".md$")\n"
@@ -84,7 +95,7 @@ main() {
     # remove corresponding html files for removed markdown files
     if [ "$vfold" ]; then
         vremovelist="$vremovelist$(echo -e "$vfold" | srctodst | mdfilenametohtml)\n"
-        mv $vremovelist "$vroot/$vbackupdirname"
+        echo -e "$vremovelist" | xargs -I {} rm {}
     fi
     if (test "$vcopylist"); then
         # remove blank lines
@@ -93,49 +104,106 @@ main() {
             mkdir -p "$(echo "$i" | srctodst)"
         done
         echo -e "$vcopylist" | sort -u | while read -r i; do
-            cp $i "$(echo $i | srctodst)"
+            cp "$i" "$(echo "$i" | srctodst)"
         done
-        echo -e "$vcopylist" | sort -u | grep ".html" | while read -r i; do
-            mv $i "$vroot/$vbackupdirname"
-        done
-        mv $vdotfiles "$vdotfiles.old"
         echo -e "$vcurrentfilelist" > "$vdotfiles"
-        if (test "$visrelease" = "y"); then
-            rm "$vdotfiles.tmp" "$vdotfiles"
-            cp "$vstyle" "$vroot/$vdstname"
+        if test "$vresiduefilelist"; then
+            echo -e "$vresiduefilelist" | xargs -I {} rm {} 2>/dev/null
         fi
+        genrss
     else
         echo "No changes."
     fi
     # end profiler
-    # set +x
-    # exec 2>&3 3>&-
+    if (echo "$vargs" | grep -q 'p'); then
+        set +x
+        exec 2>&3 3>&-
+    fi
 }
 die() {
     echo "$1" >&2 && exit 1
 }
 debug_echo() {
-    echo $vargs | grep -q 'v' && echo -e "[debug] $1" >&2
+    echo "$vargs" | grep -q 'v' && echo -e "[debug] $1" >&2
+}
+script_init() {
+    echo "No config file found. Creating one at $vconfig."
+    cat >"$vconfig" <<EOF
+# The name of your blog (website)
+vblogname="$(whoami)"
+
+# The topmost directory of your website. 
+# Contains the source and destination directories.
+vroot="$(pwd)"
+
+# The name of your source directory
+vsrcname="src"
+
+# The name of your destination directory
+vdstname="pages"
+
+# The name of your header file. Header files are included at the top of all pages.
+vheadername="header"
+
+# The name of your footer file. Footer files are included at the bottom of all pages.
+vfootername="footer"
+
+# The markdown converter command you would be using. For pandoc,
+# the command would be "pandoc -f markdown -t html".
+vmdconvcommand="pandoc -f markdown -t html"
+
+### RSS config ###
+
+# The URL of your site
+vrssurl="https://ltlnx.tw"
+
+# The title of your site
+vrsstitle="Blog of ltlnx - All pages"
+
+# The description of the site
+vrssdesc="All pages on the website of ltlnx"
+
+# The language of the site (defined by your language code, e.g. "en" for English,
+# with an optional region code, e.g. "zh-tw" for Chinese in Taiwan)
+vrsslang="zh-tw"
+
+# The copyright of your RSS file content
+vrsscopyright="Wen-Wei Kao"
+
+# The URL of the RSS feed
+vrssfeedurl="https://ltlnx.tw/rssfeed.xml"
+EOF
+    echo "Please adjust the config file to your needs before running the script again."
+    exit 0
+}
+run_tests() {
+    test "$vblogname" || echo "vblogname not set."
+    test -d "$vroot" || echo "The topmost directory $vroot does not exist."
+    test -d "$vroot"/"$vsrcname" || echo "The source directory $vroot/$vsrcname does not exist."
+    test -d "$vroot"/"$vdstname" || echo "The destination directory $vroot/$vdstname does not exist."
+    test "$vheadername" || echo "vheadername not set."
+    test -f "$vroot"/"$vheadername" || echo "Header file not found. Please put a header file under the topmost directory."
+    test "$vfootername" || echo "vfootername not set."
+    test -f "$vroot"/"$vfootername" || echo "Footer file not found. Please put a footer file under the topmost directory."
+    which $(echo "$vmdconvcommand" | cut -d ' ' -f 1) >/dev/null || echo "No supported Markdown parser found. Please set it up in the config file."
 }
 argsparse() {
     local args=$(echo "$1" | grep -o "[[:alnum:]]" | sort -u) \
         || die "Failed to parse arguments"
-    echo $args | grep -q "h" && print_help && exit 0
-    echo $args && return 0 || return 1
+    echo "$args" | grep -q "h" && print_help && exit 0
+    echo "$args" && return 0 || return 1
 }
 # for the two find functions we use $@ instead of $1 to preserve arguments
 # passed to the function
 find_exclusion() {
-    find $@ -type f \
+    find "$@" -type f \
         -not -path "*/beta/*" -not -path "*/beta" \
-        -not -path "*/.*" -not -name "desc" -not -name "index.md"
+        -not -path "*/notes/*" -not -path "*/notes" \
+        -not -path "*/.*" -not -name "desc" \
+        -not -name "index.md" -not -name "$vheadername" -not -name "$vfootername"
 }
 find_dirs_exclusion() {
-    find $@ -type d -mindepth 1 -not -path "*/res/*" -not -path "*/res" -not -path "*/.*"
-}
-test_dirs() {
-    test -d "$vroot/$vsrcname" || return 1
-    test -d "$vroot/$vdstname" || return 1
+    find "$@" -type d -mindepth 1 -not -path "*/res/*" -not -path "*/res" -not -path "*/.*"
 }
 get_dirs() {
     echo -e "$1" | rev | cut -d '/' -f 2- | rev | sort -u
@@ -154,36 +222,38 @@ srctodst() {
 }
 mdfilenametohtml() {
     read inp
-    echo -e "$(echo -e "$inp" | grep -Po ".*(?=.md)").html"
+    echo -e "$(echo "$inp" | sed 's/.md$/.html/g')"
 }
 build_fileindex() {
-    cd $1
+    cd "$1"
     debug_echo "Building fileindex for $1"
     local fileindex=""
     for mdfile in \
-        $(find $1 -mindepth 1 -maxdepth 1 -path "*.md" -not -name "index.md"); do
-        local D=$(cat $mdfile | grep -Po '^[\_]*Last [[:alpha:]]*: \K[^\_]*' \
+        $(find "$1" -mindepth 1 -maxdepth 1 -path "*.md" -not -name "index.md" -not -path "*/.*"); do
+        local D=$(cat "$mdfile" | grep -Po '^[\_]*Last [[:alpha:]]*: \K[^\_]*' \
             || echo "(No date)")
-        local T=$(head -n 5 $mdfile | grep -Po "^# \K.*$" | rmhtmltags)
-        local P=$(basename $mdfile | mdfilenametohtml | sed "s|^\./||g")
+        local T=$(head -n 5 "$mdfile" | grep -Po "^# \K.*$" | rmhtmltags)
+        local P=$(basename "$mdfile" | mdfilenametohtml | sed "s|^\./||g")
         fileindex="$fileindex- <span class=\"date\">$D</span>[$T]($P)\n"
     done
     test "$fileindex" && echo -e "$fileindex" | sort -r -n
 }
 build_index() {
-    cd $1
+    cd "$1"
     debug_echo "Building index for $1"
     local index="$(cat desc 2>/dev/null)" \
-        || index="# $(basename $1 | titlize)\n\nNo description.\n\n---\n"
-    for subdir in $(find_dirs_exclusion $1); do
+        || index="# $(basename "$1" | titlize)\n\nNo description.\n\n---\n"
+    for subdir in $(find_dirs_exclusion "$1" | sort); do
         debug_echo "Creating sub-index for $subdir"
         test -f "$subdir/.fileindex" || continue
-        index="$index\n\n### $(basename $subdir | titlize)\n"
+        index="$index\n\n### $(basename "$subdir" | titlize)\n"
         index="$index$(head -n 10 "$subdir/.fileindex" | \
-            sed "s|](|]($(basename $subdir)/|g")"
-        index="$index$(test $(wc -l < "$subdir/.fileindex") -gt 11 \
-            && echo "\n\n[More →]" \
-            || echo "\n\n[Dedicated page →]")($(basename $subdir)/index.html)"
+            sed "s|](|]($(basename "$subdir")/|g")"
+        index="$index$(if [ $(wc -l < "$subdir/.fileindex") -gt 11 ]; then
+            echo "\n\n[More →]"
+        else
+            echo "\n\n[Dedicated page →]"
+        fi)($(basename "$subdir")/index.html)"
     done
     test $(wc -l <.fileindex) -gt 0 &&
         if (echo -e "$index" | tail -n 1 | grep -q "→]"); then
@@ -198,27 +268,70 @@ build_index() {
 gennav() {
     if (echo "$1" | grep -qv "index.md"); then
         dotdot="index.html"
-        echo $(echo -e "$(for i in $(echo $1 | rev | cut -d '/' -f 2- | rev | sed "s|$vroot/$vsrcname||" | grep -o "\b[^/]*" | tac); do echo "[$(echo $i | titlize)]($dotdot) >"; dotdot="../$dotdot"; done; echo "[$vblogname]($dotdot) >")" | tac) | $vmdconvcommand
+        echo $(echo -e "$(for i in $(echo "$1" | rev | cut -d '/' -f 2- | rev | sed "s|$vroot/$vsrcname||" | grep -o "\b[^/]*" | tac); do echo "[$(echo "$i" | titlize)]($dotdot) >"; dotdot="../$dotdot"; done; echo "[$vblogname]($dotdot) >")" | tac) | $vmdconvcommand
     elif [ "$1" != "$vroot/$vsrcname/index.md" ]; then
         dotdot="../index.html"
-        echo $(echo -e "$(for i in $(echo $1 | rev | cut -d '/' -f 3- | rev | sed "s|$vroot/$vsrcname||" | grep -o "\b[^/]*" | tac); do echo "[$(echo $i | titlize)]($dotdot) >"; dotdot="../$dotdot"; done; echo "[$vblogname]($dotdot) >")" | tac; echo "**$(echo $1 | rev | cut -d '/' -f 2 | rev | titlize)**") | $vmdconvcommand
+        echo $(echo -e "$(for i in $(echo "$1" | rev | cut -d '/' -f 3- | rev | sed "s|$vroot/$vsrcname||" | grep -o "\b[^/]*" | tac); do echo "[$(echo "$i" | titlize)]($dotdot) >"; dotdot="../$dotdot"; done; echo "[$vblogname]($dotdot) >")" | tac; echo "**$(echo "$1" | rev | cut -d '/' -f 2 | rev | titlize)**") | $vmdconvcommand
     else
         echo "<p><strong>$vblogname</strong></p>"
     fi
 }
-text_substitutions() {
-    # text subs for every post go here
-    cat "$1"
+parse_shortcodes() {
+    parsed_content="$1"
+    for i in $(echo -e "$parsed_content" | grep -Po "{{\K.*?(?=}})"); do
+        parsed_content=$(echo -e "$parsed_content" | sed "s|{{$i}}|$(cat "$vroot"/shortcodes/"$i")|")
+    done
+    echo -e "$parsed_content"
 }
 mdtohtml() {
     local content=""
-    page_title="$(head -n 5 $1 | grep -Po "^# \K.*$" | rmhtmltags)" || return 1
-    content="$content$(cat $vheader | sed "s|<title>|<title>$page_title|g")\n" || return 1
-    content="$content$(gennav $1)\n" || return 1
-    # content="$content$(text_substitutions $1 | pandoc -f markdown -t html)\n" || return 1
-    content="$content$(text_substitutions $1 | $vmdconvcommand)\n" || return 1
-    content="$content$(cat $vfooter)\n" || return 1
-    echo -e "$content" || return 1
+    page_title="$(head -n 5 "$1" | grep -Po "^# \K.*$" | rmhtmltags) - Blog of ltlnx" || return 1
+    content="$content$(cat "$vheader" | sed "s|<title>|<title>$page_title|g; s|<p id=\"nav\"></p>|$(echo $(gennav "$1" | sed 's/<p>/<p id="nav">/;s/\&/\\\&/g'))|g")\n" || return 1
+    content="$content$($vmdconvcommand < "$1")\n" || return 1
+    content="$content$(cat "$vfooter")\n" || return 1
+    echo -e "$(parse_shortcodes "$content")" || return 1
+}
+genrss() {
+    # rss item generation
+    mv "$vroot/rssitems" "$vroot/rssitems.old"
+    for mdfile in $(echo -e "$vcurrentfilelist" | grep ".md$"); do
+        # get info of markdown file
+        it_title="$(head -n 5 "$mdfile" | grep -Po "^# \K.*$" | rmhtmltags)"
+        it_guid="$(echo "$mdfile" | grep -Po "^$vroot/$vsrcname/\K.*$" | mdfilenametohtml)"
+        it_link="https://ltlnx.tw/$it_guid"
+        it_pubDate="$(date -d "$(cat "$mdfile" | grep -Po '^[\_]*Last [[:alpha:]]*: \K[^\_]*' || echo "1970-01-01")" -R)"
+        it_description="$(cat "$mdfile" | grep -v "^> " | grep -v "^# " | pandoc -f markdown -t plain | sed -n "/^/,/^$/p; /^$/q")"
+        cat >> "$vroot/rssitems" <<EOF 
+<item>
+ <title>$it_title</title>
+ <link>$it_link</link>
+ <guid isPermaLink="false">$it_guid</guid>
+ <pubDate>$it_pubDate</pubDate>
+ <description><![CDATA[$it_description]]></description>
+</item>
+EOF
+    done
+    builddate=$(date -R)
+    # initial overhead
+    cat > "$vroot/$vdstname/rssfeed.xml" <<EOF
+<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>$vrsstitle</title>
+    <link>$vrssurl</link>
+    <description>$vrssdesc</description>
+    <language>$vrsslang</language>
+    <copyright>$vrsscopyright</copyright>
+    <lastBuildDate>$builddate</lastBuildDate>
+    <atom:link href="$vrssfeedurl" rel="self" type="application/rss+xml" />
+EOF
+    # items
+    cat "$vroot/rssitems" >> "$vroot/$vdstname/rssfeed.xml" 
+    # ending
+    cat >> "$vroot/$vdstname/rssfeed.xml" <<EOF
+  </channel>
+</rss>
+EOF
 }
 
 main "$@"
